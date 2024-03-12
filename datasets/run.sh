6c9cc6bba42553ee8a09341213c1a99cb58dd3ea
Nguyen Khang <khanghn1120@gmail.com>
2024-03-12 10:47:55 +0700

    Update run.sh

--- datasets/run.sh
+++ datasets/run.sh
@@ -1,7 +1,12 @@
+# Convert crowhuman annotations to COCO format
 python crowdhuman_to_coco.py --data_path /home/nguyenkhang/Documents/YOLOv8/datasets/crowdhuman/
 
-python coco_to_yolo.py --data_path /home/nguyenkhang/Documents/YOLOv8/datasets/crowdhuman/ \
+# Convert crowhuman annotations COCO format to YOLO format
+python crowhuman_to_yolo.py --data_path /home/nguyenkhang/Documents/YOLOv8/datasets/crowdhuman/ \
                                 --split val train --dataset crowdhuman -np 0 
 
+#Convert COCO annotations to YOLO format
+python coco_to_yolo.py 
+
 # # Combine dataset
-# python mix_mot_ch.py --paths /home/nguyenkhang/Documents/YOLOv8/datasets --out_path /home/nguyenkhang/Documents/YOLOv8/datasets/coco_crow
+# python mix_coco_crow.py --paths /home/nguyenkhang/Documents/YOLOv8/datasets --out_path /home/nguyenkhang/Documents/YOLOv8/datasets/coco_crow
