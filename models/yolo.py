57b74ee9a703e2424bf254b1af6491844dc1c7a1
khanghn <khanghn1120@gmail.com>
2024-03-12 20:15:09 +0700

    yolo

--- models/yolo.py
+++ models/yolo.py
@@ -72,7 +72,8 @@ class V8_Detect(nn.Module):
         box, cls = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2).split((self.reg_max * 4, self.nc), 1)
         dbox = dist2bbox(self.dfl(box), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
         y = torch.cat((dbox, cls.sigmoid()), 1)
-        return y if self.export else (y, x)
+        # return y if self.export else (y, x)
+        return (y, x)
 
     def bias_init(self):
         # Initialize Detect() biases, WARNING: requires stride availability
