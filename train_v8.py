ee119cba84241e71ab8f04889889ed4decf82267
khanghn <khanghn1120@gmail.com>
2024-03-16 00:03:05 +0700

    2gpus

--- train_v8.py
+++ train_v8.py
@@ -2,4 +2,4 @@ from engine import YOLO
 
 
 model = YOLO('yolov8m.yaml')
-model.train(data='coco-crow.yaml', epochs=10, batch=32, imgsz=640, device=0,1)
+model.train(data='coco-crow.yaml', epochs=10, batch=32, imgsz=640, device=[0,1])
