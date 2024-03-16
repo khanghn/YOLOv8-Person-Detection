253a3c48e66dec52b174b3f04ba2330967ac689d
khanghn <khanghn1120@gmail.com>
2024-03-16 23:15:35 +0700

    train2

--- train_v8.py
+++ train_v8.py
@@ -2,4 +2,4 @@ from engine import YOLO
 
 
 model = YOLO('yolov8m.yaml')
-model.train(data='coco-crow.yaml', epochs=50, batch=16, imgsz=640)
+model.train(data='coco-crow.yaml', epochs=50, batch=-1, imgsz=640)
