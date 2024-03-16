ceb5e77e33f9717cfbbddb5e0824c5d7d4e9e48d
khanghn <khanghn1120@gmail.com>
2024-03-16 22:05:03 +0700

    train

--- predict.py
+++ predict.py
@@ -7,6 +7,9 @@ from engine.models.yolo.detect import DetectionPredictor
 # model.predict(data='coco-crow.yaml', epochs=10, batch=-1, imgsz=640)
 
 
-args = dict(model='/home/nguyenkhang/Downloads/best (1).pt', source='/home/nguyenkhang/Documents/YOLOv8/datasets/coco_crow/images/val')
+args = dict(model='/home/nguyenkhang/Downloads/best (1).pt', 
+            source='/home/nguyenkhang/Documents/YOLOv8/datasets/coco_crow/images/val',
+            save_dir= '/home/nguyenkhang/Documents/YOLOv8/datasets/coco_crow/results/'
+        )
 predictor = DetectionPredictor(overrides=args)
 predictor.predict_cli()
--- train_v8.py
+++ train_v8.py
@@ -1,6 +1,5 @@
 from engine import YOLO
 
 
-# model = YOLO('yolov8m.yaml')
-model = YOLO('/kaggle/input/bestt/pytorch/model/1/best (1).pt')
-model.train(data='coco-crow.yaml', resume=True, epochs=50, batch=-1, imgsz=640)
+model = YOLO('yolov8m.yaml')
+model.train(data='coco-crow.yaml', epochs=50, batch=16, imgsz=640)
